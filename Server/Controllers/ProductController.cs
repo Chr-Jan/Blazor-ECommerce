988d464c6e43a68e500dcbf5222b90b63f9b1c45
Christoffer Jansson <christofferjansson1@gmail.com>
2023-11-16 20:11:49 +0100

    single product controller

--- Server/Controllers/ProductController.cs
+++ Server/Controllers/ProductController.cs
@@ -16,10 +16,19 @@ namespace BlazorECommerce.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProducts()
         {
             var result = await _productService.GetProductsAsync();
             return Ok(result);
         }
+
+        [HttpGet]
+        [Route("{productId}")]
+        public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+        {
+            var result = await _productService.GetProductAsync(productId);
+            return Ok(result);
+        }
+
     }
 }
