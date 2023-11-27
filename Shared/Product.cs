b928762917edbc7c8745c238bd479d909524fc42
Christoffer Jansson <christofferjansson1@gmail.com>
2023-11-27 18:28:23 +0100

    ProductVariant

--- Shared/Product.cs
+++ Shared/Product.cs
@@ -14,8 +14,6 @@ namespace BlazorECommerce.Shared
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal Price { get; set; }
         public Category? Category { get; set; }
         
         //specify the category id (foreign key)
