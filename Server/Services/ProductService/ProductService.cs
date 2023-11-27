5d3e49d8d7ba44d5764a3560ffc00b950f54a041
Christoffer Jansson <christofferjansson1@gmail.com>
2023-11-27 19:59:42 +0100

    ProductService Server Update

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -9,10 +9,13 @@
             _context = context;
         }
 
-        public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        public async Task<ServiceResponse<Product>> GetProductAsync(int id)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == id);
             if (product == null)
             {
                 response.Success = false;
@@ -28,6 +31,9 @@
 
         public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
         {
+            var products = await _context.Products
+                .Include(p=>p.Variants)
+                .ToListAsync();
             var response = new ServiceResponse<List<Product>>
             {
                 Data = await _context.Products.ToListAsync()
@@ -41,7 +47,11 @@
             var reponse = new ServiceResponse<List<Product>>
             {
                 Data = await _context.Products
-                .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                .Where(p => p.Category.Url
+                .ToLower()
+                .Equals(categoryUrl
+                .ToLower()))
+                .Include(p => p.Variants)
                 .ToListAsync()
             };
 
